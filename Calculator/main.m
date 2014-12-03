a44409aa707d44a164c6db8a6a650a9bd2fde8b6
charlesaroutiounian <sierpinski>
2014-12-03 14:30:46 +0100

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Charles Aroutiounian on 03/12/14.
+//  Copyright (c) 2014 Charles Aroutiounian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
